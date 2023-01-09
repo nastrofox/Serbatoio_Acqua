f1a60152f1ae1a51a5288cadc3ef065a634f7689
Crotti.19315 <Crotti.19315@LABI1-6PC28>
2023-01-09 08:34:17 +0100

    Aggiungere i file di progetto.

---
+++ Serbatoio_D_Acqua/serbatoio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serbatoio_D_Acqua
+{
+    internal class serbatoio
+    {
+        private string _numeroseriale;
+        private string _modello;
+        private string _produttore;
+        private float _quantmax;
+        private float _quantmin;
+
+
+
+    }
+}
